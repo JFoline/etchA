efe061277debcfbec09a7401f406c295cf201e8b
John Foline <john.foline@gmail.com>
2018-03-09 13:04:57 -0600

    Mouseover changes cell to black

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const x = 16;
+const x = 5;
 const container = document.querySelector('#container');
+const clear =document.querySelector('#clear');
 
 container.style.setProperty('grid-template-columns', `repeat(${x}, 1fr)`);
 
 for (let i = 0; i < x * x; i++) {
   const singleCell = document.createElement('div');
-  singleCell.setAttribute('style','border-color:grey; border-style:inset;');
+  singleCell.setAttribute('style','border-style:outset;');
   container.appendChild(singleCell);
+
+  singleCell.onmouseenter = function(){
+  singleCell.setAttribute('style','border-style:inset; background-color:black;');
+  };
+
 }
