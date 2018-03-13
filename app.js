a72e1ab752ea025dd191d23d3e51b94d9d241a4d
John Foline <john.foline@gmail.com>
2018-03-12 19:02:44 -0500

    Clear screen working

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const x = 5;
-const container = document.querySelector('#container');
-const clear =document.querySelector('#clear');
+let x = 4;
+let container = document.querySelector('#container');
+let clearCell =document.querySelector('#clear');
+growHost();
 
+function growHost(){
+  for (let i = 0; i < x * x; i++) {
+    let singleCell = document.createElement('div');
+    singleCell.classList.add('emptyCell');
+    container.appendChild(singleCell);
+
+    singleCell.onmouseenter = function(){
+    singleCell.classList.add('fullCell');
+    };
+  }
 container.style.setProperty('grid-template-columns', `repeat(${x}, 1fr)`);
 
-for (let i = 0; i < x * x; i++) {
-  const singleCell = document.createElement('div');
-  singleCell.setAttribute('style','border-style:outset;');
-  container.appendChild(singleCell);
+};
 
-  singleCell.onmouseenter = function(){
-  singleCell.setAttribute('style','border-style:inset; background-color:black;');
-  };
-
+function killColony(){
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
+
+clearCell.addEventListener('click', function(){
+killColony();
+growHost();
+});
